63d1288ed6a527308b2ecfe00fc6a3e942d13d4f
mejdichebbi <mejdichebbi2012@gmail.com>
2020-04-18 17:28:59 +0200

    j'ai ajouter url de l'index

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store.apps.StoreConfig'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
