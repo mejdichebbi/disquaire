10538804f23f3e07400496a975259d77aae19c88
mejdichebbi <mejdichebbi2012@gmail.com>
2020-04-19 01:41:04 +0100

    Mise à jour

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from .import views
+
 urlpatterns = [
-    #url(r'^$', views.index)
+    url(r'^$', views.listing)
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from .models import ALBUMS
+
+
 def index(request):
     message = "Salut tout le monde"
+    return HttpResponse(message)
+
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
